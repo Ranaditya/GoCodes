4cc61bb3e4cc88656d9553eff8528a7666a90a26
Ranaditya Mukherjee <ranaditya2011@gmail.com>
2020-04-26 16:58:53 +0100

    Update main.go
    - create a struct that holds person fields
    - create a struct that holds secret agent fields and embeds person type
    - attach a method to person: pSpeak
    - attach a method to secret agent: saSpeak
    - create a variable of type person
    - create a variable of type secret agent
    - print a field from person
    - run pSpeak attached to the variable of type person
    - print a field from secret agent
    - run saSpeak attached to the variable of type secret agent
    - run pSpeak attached to the variable of type secret agent

--- HandsOn2/main.go
+++ HandsOn2/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type person struct {
+	fname string
+	lname string
+}
+type secretagent struct {
+	p      person
+	l2kill bool
+}
+
+func (p person) pSpeak() {
+	fmt.Println(p.fname, p.lname, "says how r u doing?")
+}
+
+func (sa secretagent) saSpeak() {
+	fmt.Println("secret agent", sa.p.fname, sa.p.lname, "has l2kill", sa.l2kill)
+}
+
+func main() {
+	p := person{
+		"John", "Snow",
+	}
+	s := secretagent{
+		person{"James", "Bond"},
+		true,
+	}
+	p.pSpeak()
+	s.saSpeak()
+	s.p.pSpeak()
 }
